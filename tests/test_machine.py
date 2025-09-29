78e389de96343d8646659a7303e121f9c83cec88
Jorge Martinez-Palomera <jorgemarpa@ug.uchile.cl>
2025-09-29 11:47:28 -0400

    adding basic pytest

---
+++ tests/test_machine.py
@@ -0,0 +1,16 @@
+"""
+Actual test won't be possible as copying image files to GitHub repo will use too much
+memory.
+"""
+
+import pytest
+from roman_lcs import Machine
+
+
+def test_machine():
+    """
+    Dummy function to test error when no files are provided
+    """
+    with pytest.raises(TypeError) as excinfo:
+        _ = Machine()
+    assert excinfo.type == TypeError
