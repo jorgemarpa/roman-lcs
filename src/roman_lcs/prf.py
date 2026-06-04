ec4d282467e8b2e983e2021a2c906990ee4ca708
Jorge Martinez-Palomera <jorgemarpa@ug.uchile.cl>
2026-06-04 15:53:58 -0400

    adding stpsf ePSF library code

--- src/roman_lcs/prf.py
+++ src/roman_lcs/prf.py
@@ -6,8 +6,12 @@ from typing import Optional
 
 import numpy as np
 from astropy.io import fits
-from astropy.convolution import Box2DKernel, convolve
+
+# from astropy.convolution import Box2DKernel, convolve
 from scipy import interpolate
+from scipy.ndimage import convolve, gaussian_filter, uniform_filter
+from stpsf import roman
+from tqdm import tqdm
 
 from . import PACKAGEDIR
 
@@ -59,6 +63,7 @@ class RomanPRF(object):
         sky_space: bool = False,
         conv: str = True,
         file_name: Optional[str] = None,
+        interp_method: str = "linear",
     ):
 
         if file_name is not None:
@@ -100,14 +105,16 @@ class RomanPRF(object):
             self.dx *= self.pixel_scale
 
         if conv:
-            box_kernel = Box2DKernel(self.oversample_factor)
-            self.prf = convolve(self.prf, box_kernel)
+            # box_kernel = Box2DKernel(self.oversample_factor)
+            # self.prf = convolve(self.prf, box_kernel)
+
+            self.prf = uniform_filter(self.prf, size=self.oversample_factor)
         
         self.prf_sum = self.prf.sum()
 
-        self._build_interpolator()
+        self._build_interpolator(method=interp_method)
 
-    def _build_interpolator(self, method: str = "cubic"):
+    def _build_interpolator(self, method: str = "linear"):
         """
         Build 2D interpolation function for PSF model.
         
@@ -178,3 +185,113 @@ class RomanPRF(object):
 
 
 
+
+def apply_wfi_detector_effects(
+    psf_data, ipc_center=0.9119, symmetric_ipc=True, diffuse=False, diffusion_sigma=0.3
+):
+    """
+    Applies Inter-Pixel Capacitance (IPC) and Charge Diffusion to a PSF image.
+    
+    Parameters:
+    - psf_data: 2D numpy array (the monochromatic PSF)
+    - ipc_alpha: Coupling coefficient for nearest neighbors (typical WFI ~0.008)
+    - diffusion_sigma: Sigma for Gaussian charge diffusion in pixels.
+    """
+    # apply charge diffusion (Gaussian blur). this is already incorporated in latest
+    # version of STPSF
+    if diffuse:
+        psf_data = gaussian_filter(psf_data, sigma=diffusion_sigma)
+    
+    # apply inter-pixel capacitance (IPC)
+    # Standard 3x3 IPC kernel
+    # See Roman WFI detector documentation for updated alpha values per SCA
+    if symmetric_ipc:
+        # using central value and symmetric matrix given in Roman Doc:
+        # https://roman.ipac.caltech.edu/page/param-db
+        ipc_res = (1 - ipc_center)/4
+        ipc_kernel = np.array(
+            [[0, ipc_res, 0], [ipc_res, ipc_center, ipc_res], [0, ipc_res, 0]]
+        )
+    else:
+        # taken from:
+        # https://roman.gsfc.nasa.gov/science/RRI/Roman_WFI_Reference_Information_20210125.pdf
+        ipc_kernel = np.array(
+            [[0.21, 1.90, 0.21], [1.84, 91.19, 1.82], [0.22, 1.98, 0.22]]
+        ) / 100.0
+
+    
+    psf_data = convolve(psf_data, ipc_kernel)
+    return psf_data
+
+def generate_chromatic_psf_library(
+    filter_name="F146",
+    detector="WFI01",
+    positions=[(1000, 1000)],
+    step_um=0.05,
+    oversample=7,
+    dir_path=None,
+):
+    """
+    Generates a library of monochromatic PRFs across the filter bandpass.
+    """
+    wfi = roman.WFI()
+    wfi.filter = filter_name
+    wfi.detector = detector
+    wfi.options["parity"] = "odd"
+    if isinstance(positions, tuple):
+        positions = [positions]
+    
+    # define wavelength grid based on F146 (approx 0.93 - 2.00 um)
+    # using microns as input, converting to meters for STPSF
+    wavelens_um = np.arange(0.95, 2.05, step_um)
+    
+    psf_library = {}
+
+    print(f"Generating PSF library for {filter_name} {detector}")
+    print(f"Wavelength range: {wavelens_um[0]:.2f} - {wavelens_um[-1]:.2f} um, step: {step_um:.2f} um")
+    print(f"Total Positions: {len(positions)}")
+    for pos in tqdm(positions, total=len(positions), desc=f"Processing positions"):
+        wfi.detector_position = pos
+        pos_key = f"{pos[0]}_{pos[1]}"
+        psf_library[pos_key] = {}
+        for wl in tqdm(
+            wavelens_um,
+            total=len(wavelens_um),
+            desc=f"Processing wavelength",
+            leave=False,
+        ):
+            wl_meters = wl * 1e-6
+            
+            # calculate monochromatic PSF
+            # 'fov_pixels' should be sized to capture the wings for high-precision photometry
+            hdul = wfi.calc_psf(
+                monochromatic=wl_meters, fov_pixels=101, oversample=oversample
+            )
+            
+            # ext 0 is the oversampled PSF
+            # ext 1 is the detector sampled PSF
+            # ext 2 is the oversampled distorted PSF, including charge diffusion
+            # ext 3 is the detector sampled distorted PSF, including charge diffusion
+            
+            # apply detector effects (IPC & diffusion) this should be applied to 
+            # detector sampled PSF
+            over_prf_dist = apply_wfi_detector_effects(hdul[2].data, diffuse=False)
+            det_prf_dist = apply_wfi_detector_effects(hdul[3].data, diffuse=False)
+
+            hdul[2].data = over_prf_dist
+            hdul[2].header.set("IPCTYPE", "symmetric", "IPC kernel definition", after="CHDFSIGM")
+            hdul[2].header.set("IPCENT", 0.9119, "IPC kernel definition", after="IPCTYPE")
+            hdul[2].header["HISTORY"] = "Applied IPC"
+            
+            hdul[3].data = det_prf_dist
+            hdul[3].header.set("IPCTYPE", "symmetric", "IPC kernel definition", after="CHDFSIGM")
+            hdul[3].header.set("IPCENT", 0.9119, "IPC kernel definition", after="IPCTYPE")
+            hdul[3].header["HISTORY"] = "Applied inter-pixel capacitance (IPC)"
+
+            if dir_path is not None:
+                output_file = os.path.join(dir_path, f"psf_{filter_name}_{detector}_{pos_key}_{wl:.2f}um.fits")
+                hdul.writeto(output_file, overwrite=True)
+            
+            psf_library[pos_key][wl] = over_prf_dist
+
+    return psf_library
